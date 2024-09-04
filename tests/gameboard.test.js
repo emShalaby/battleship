c635237aefb7df243ef2650d668068b68aba5750
emShalaby <medo.dice@gmail.com>
2024-09-04 10:45:37 +0300

    refactor gameboard testing code

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -31,36 +31,43 @@ describe("gameBoard", () => {
 
   test("set location method should correctly update a ships location and update the map", () => {
     testBoard.setLocation(
-      testBoard.ships.find((ship) => ship.id == testShips[0].id),
+      testBoard.ships.find((ship) => ship.id == ship1.id),
       [[0, 0]]
     );
     expect(
-      testBoard.ships.find((ship) => ship.id == testShips[0].id).location
+      testBoard.ships.find((ship) => ship.id == ship1.id).location
     ).toEqual([[0, 0]]);
-    expect(testBoard.map[0][0]).toBe(testShips[0].id);
+    expect(testBoard.map[0][0]).toBe(ship1.id);
   });
-  test("game board should recieve a missed attack correctly and update map", () => {
-    testBoard.recieveAttack([0, 0]);
-    expect(testBoard.map[0][0]).toBe("missed");
-  });
-  test("game board should update correctly when a ship is hit", () => {
-    testBoard.setLocation(
-      testBoard.ships.find((ship) => ship.id == testShips[0].id),
-      [[0, 0]]
-    );
-    expect(testBoard.map[0][0]).toBe(testBoard.ships[0].id);
-    testBoard.recieveAttack([0, 0]);
 
-    expect(testBoard.map[0][0]).toBe(testBoard.ships[0].id + "hit");
-  });
-  test("a ship should increase its hits when a recieved attack lands on a ship", () => {
-    testBoard.setLocation(
-      testBoard.ships.find((ship) => ship.id == testShips[0].id),
-      [[0, 0]]
-    );
-    testBoard.recieveAttack([0, 0]);
-    expect(
-      testBoard.ships.find((ship) => ship.id == testShips[0].id).hitTimes
-    ).toBe(1);
+  describe("hit detection", () => {
+    beforeEach(() => {
+      testBoard.setLocation(
+        testBoard.ships.find((ship) => ship.id == ship1.id),
+        [[0, 0]]
+      );
+    });
+    test("game board should recieve a missed attack correctly and update map", () => {
+      testBoard.recieveAttack([1, 1]);
+      expect(testBoard.map[1][1]).toBe("missed");
+    });
+    test("game board should update correctly when a ship is hit", () => {
+      expect(testBoard.map[0][0]).toBe(testBoard.ships[0].id);
+      testBoard.recieveAttack([0, 0]);
+
+      expect(testBoard.map[0][0]).toBe(testBoard.ships[0].id + "hit");
+    });
+    test("a ship should increase its hits when a recieved attack lands on a ship", () => {
+      testBoard.recieveAttack([0, 0]);
+      expect(testBoard.ships.find((ship) => ship.id == ship1.id).hitTimes).toBe(
+        1
+      );
+    });
+    test("recieved attack should trigger a status change when hittimes is equal to length", () => {
+      testBoard.recieveAttack([0, 0]);
+      expect(testBoard.ships.find((ship) => ship.id == ship1.id).status).toBe(
+        "dead"
+      );
+    });
   });
 });
